7d6c1722f5cf4b647add1d06b648415615884d27
hreich29@riverdale.edu <hreich29@riverdale.edu>
2023-12-20 15:34:27 -0500

    no. comment.

--- src/robosim/RoboMap.java
+++ src/robosim/RoboMap.java
@@ -9,18 +9,22 @@ public class RoboMap {
     public int[] roboPos;
     public RoboMap(int[] dimensions, int[] roboStartPos) {
         try {
-            MapObject[][] pmap = new MapObject[dimensions[0]][dimensions[1]];
-            for (int i = 0; i< pmap.length; ++i) {
-                for (int j = 0; j<pmap[i].length; ++j) {
-                    pmap[i][j] = new Air();
-                }
-            }
+            MapObject[][] pmap = initializeMap(dimensions);
             this.RMap = new FTCMap(pmap);
             this.RMap.FMap[roboStartPos[0]][roboStartPos[1]] = new Robot();
             this.roboPos = roboStartPos;
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+    public MapObject[][] initializeMap(int[] dimensions) {
+        MapObject[][] map = new MapObject[dimensions[0]][dimensions[1]];
+        for (int i = 0; i < dimensions[0]; i++) {
+            for (int j = 0; j < dimensions[1]; j++) {
+                map[i][j] = new Air();
+            }
+        }
+        return map;
     }
     public void moveRobot(DirectionVector dvector) {
         RobotMovement rm = new RobotMovement();
